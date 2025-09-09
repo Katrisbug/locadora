d0b82d4f6048878cd0937c74698848dae083483e
Katrisbug <bonetkatrina@gmail.com>
2025-09-09 10:39:46 -0300

    alugar e devolver itens

--- classes.py
+++ classes.py
@@ -5,10 +5,43 @@ class Itens:
         self.__disponivel = True
 
     def alugar (self):
-        pass
+        if self.__disponivel:
+            self.__disponivel : False
+            print(f'O item {self.__titulo} alugado com sucesso!')
+        else:
+            print(f'O item {self.__titulo} não esta disponivel para alugar.')
+    
 
     def devolver (self):
+        if not self.__disponivel:
+            self.__disponivel = True
+            print(f'O item {self.__titulo} foi devolvido com sucesso.')
+        else:
+            print(f'O item {self.__titulo} está disponível.')
+
+class Filme (Itens):
+    def __init__(self, codigo, titulo, genero, duracao):
+        self.__genero = genero
+        self.__duracao = duracao
+
+class Jogo (Itens):
+    def __init__(self, codigo, titulo, plataforma, faixa_etaria):
+        self.__plataforma = plataforma
+        self.__faixa_etaria = faixa_etaria
+
+class Clientes:
+    def __init__(self, nome, cpf, itens_locados):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__itens_locados = []
+
+    def locar (Itens):
         pass
+
+    def devolver (Itens):
+        pass
+
+    listar_itens()
 
 class Locadora:
     def __init__ (self, ):
