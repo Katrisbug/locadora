24593bc9099f565baeff45a7adf142272e5fc201
Katrisbug <bonetkatrina@gmail.com>
2025-09-09 10:10:06 -0300

    class itens

---
+++ classes.py
@@ -0,0 +1,32 @@
+class Itens:
+    def __init__(self, codigo, titulo):
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__disponivel = True
+
+    def alugar (self):
+        pass
+
+    def devolver (self):
+        pass
+
+class Locadora:
+    def __init__ (self, ):
+        self.__clientes = []
+        self.__itens = []
+
+    def cadastro_cliente (self, ):
+        pass
+
+    def cadastro_itens (self):
+        pass
+
+    def listar_clientes (self,):
+        pass
+
+    def listar_itens (self):
+        pass
+
+
+
+    
